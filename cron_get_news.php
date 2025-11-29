a04ae5525460d2d93626e07ed36e1e07d179c176
Ilia <infinity-ex@narod.ru>
2025-11-29 17:52:47 +0300

    Update cron_get_news.php

--- cron_get_news.php
+++ cron_get_news.php
@@ -1,15 +1,5 @@
 <?php
 
-// */5 * * * * cd /var/www/ttbgrossist.com/test && php -f cron_get_news.php
-/*
-cd /var/www/ttbgrossist.com/test
-php -f cron_get_news.php
-  
-telnet localhost 11211
-flush_all
-quit
- */
-
 // block run from browser
 if ((php_sapi_name() !== 'cli')) die;
 
